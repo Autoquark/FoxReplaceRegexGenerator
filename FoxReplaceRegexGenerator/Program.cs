06e0857c0f5708a669d4cb19e3a906cb2ad604b1
Autoquark <james.a.badcoe@gmail.com>
2024-12-22 15:58:34 +0000

    Handle multiple inputs, many more rules in list

--- FoxReplaceRegexGenerator/Program.cs
+++ FoxReplaceRegexGenerator/Program.cs
@@ -1,33 +1,220 @@
-﻿using System.Text.Json;
+﻿using System.Data;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 internal static class Program
 {
     private static readonly string OutputPath = "./output/";
     private static readonly string OutputFilename = "GeneratedRules.json";
+    private static readonly char[] Vowels = ['a', 'e', 'i', 'o', 'u'];
 
     private static void Main(string[] args)
     {
         Console.WriteLine("Generating replacement rules...");
 
         var ReplacementRules = new List<ReplacementRule> {
+            // Misc nouns
             new("cheese", "hardmilk"),
+            new("milk", "cow juice", outputPlural: "cow juices"),
+            new ReplacementRule("allegedly", "probably").NoPluralHandling(),
+            new("new study", "recent rumour", "new studies") { Bidirectional = true },
+            new("rebuild", "avenge") { Bidirectional = true },
+            new("text", "scribble"),
+            new("writer", "scribbler"),
+            new("theory", "guesswork", inputPlural: "theories"),
+            new ReplacementRule("research", "messing around").NoPluralHandling(),
+            new ReplacementRule("creativity", "madness").NoPluralHandling(),
+            new("idea", "notion"),
+            new("language", "lingo"),
+            new("evidence", "ancient legends"),
+            new("hormones", "jelly beans"),
+            new("people", "folk"),
+            new("engineering", "bodging"),
+            new ReplacementRule("authority", "in-chargeness").NoPluralHandling(),
+            new("productivity", "profitability") { Bidirectional = true },
+            new("wrongly", "rightly") { Bidirectional = true },
+            new ReplacementRule("elongation", "enlengthening") { Bidirectional = true }.NoPluralHandling(),
+            new("essential", "optional") { Bidirectional = true },
+            new("mandatory", "recommended") { Bidirectional = true },
+            new("necessary", "impossible") { Bidirectional = true },
+            new("flood", "surprise bath"),
+            new("resident", "dweller"),
+            new("foot", "hand", "feet") { Bidirectional = true },
+            new("bucket", "cup") { Bidirectional = true },
+            new ReplacementRule("blood", "custard") { Bidirectional = true }.NoPluralHandling(),
+            new("group", "cult"),
+            new("representative", "deniable agent"),
+            new("communication", "interference"),
+            new("communicator", "meddler"),
+            new("won't", "will") { InputsPlural = [], Bidirectional = true },
+            new("consulted", "interrogated") { InputsPlural = [], Bidirectional = true },
+            new("consultation", "interrogation") { Bidirectional = true },
+            new("brain tumour", "headache") { Bidirectional = true },
+            new("yes", "no"),
+            new("should", "shouldn't") { Bidirectional = true},
+            new("curious", "fearful") { Bidirectional = true},
+            new("figuratively", "literally") { Bidirectional = true},
+            new("principles", "quirks") { Bidirectional = true},
+
+            // Question words
+            new ReplacementRule("why", "how").NoPluralHandling(),
+            new ReplacementRule("how", "when").NoPluralHandling(),
+            new ReplacementRule("when", "where").NoPluralHandling(),
+            new ReplacementRule("where", "what").NoPluralHandling(),
+            new ReplacementRule("what", "who").NoPluralHandling(),
+            new ReplacementRule("who", "why").NoPluralHandling(),
+
+            new("measurement", "reckoning"),
+            new("measure", "reckon"),
+
+            // Materials
+            new("concrete", "papier mache"),
+            new("concrete", "papier-mâché") { Bidirectional = true },
+
+            new("philosophy", "pondering"),
+            new("philosopher", "ponderer"),
+            new("philosophical", "ponderous"),
+
+            new("woke", "awakened") { Bidirectional = true },
+            new("blessed", "cursed") { Bidirectional = true },
+
+            // Units/measures
+            new("mile", "furlong") { Bidirectional = true },
+            new("kilometre", "league") { Bidirectional = true },
+            new("kilometer", "league") { Bidirectional = true },
+            new("litre", "dram") { Bidirectional = true },
+            new("liter", "dram") { Bidirectional = true },
+
+            // Names
+            new("charles", "charler"),
+            new("ben", "benjermy"),
+            new("benjamin", "benjermy"),
+            new("larry", "larriott"),
+
+            // Heresy
+            new("the pope", "Satan") { CapitalisationHandling = CapitalisationHandling.Ignore },
+            new("pope Francis", "Satan") { CapitalisationHandling = CapitalisationHandling.Ignore },
+            new("pope", "Satan") { CapitalisationHandling = CapitalisationHandling.Ignore },
+            new("bible", "1987 Sports Illustrated Swimsuit Issue") { CapitalisationHandling = CapitalisationHandling.Ignore },
+            new("biblical", "literary"),
+
+
+            // Animals
+            new("drone", "dog") { Bidirectional = true },
+            new("bird", "snake") { Bidirectional = true },
+
+
+            new("self driving", "possessed"),
+            new("self-driving", "possessed"),
+            new("successfully", "suddenly") { Bidirectional = true },
+            new("tension", "sexual tension"),
+            new("optimistic", "delusional") { Bidirectional = true },
+            new("CEO", "Head Honcho") { CapitalisationHandling = CapitalisationHandling.MatchExact },
+
+            // Ships
+            new("ship", "dinghy", outputPlural: "dinghies")  { Bidirectional = true },
+            new("yacht", "galleon") { Bidirectional = true },
+            new("boat", "oil tanker") { Bidirectional = true },
 
             // Durations
-            // second, minute, hour, day, week, month, year
-            new("second", "year"),
-            new("minute", "month"),
+            new("second", "year") { Bidirectional = true },
+            new("minute", "month") { Bidirectional = true },
+            new("day", "week") { Bidirectional = true },
+
+            // Grammatical
+            new("may", "must") { Bidirectional = true },
         };
 
         List<JsonReplacementGroup> phase1Groups = [];
         List<JsonReplacementGroup> phase2Groups = [];
 
+        //TODO: Handle alternate characters in rules e.g. fancy apostrope, letters with accents?
+
         foreach (var Rule in ReplacementRules)
         {
-            // Plural handling
             IEnumerable<ReplacementRule> Rules = [Rule];
 
+            /*
+            Original rule:
+            [
+                A or B -> C
+                C or D -> A
+                Aplural or Bplural -> Cplural
+                Cplural or Dplural -> Aplural
+            ]
+
+            === Stage 1: Handle plurals
+
+            Split each rule with plural handling into two rules, one for the singular inputs and one for the plural inputs
+            [
+                A or B -> C
+                C or D -> A
+            ],
+            [
+                Aplural or Bplural -> Cplural
+                Cplural or Dplural -> Aplural
+            ]
+
+            === Stage 2: Handle bidirectional
+
+            Split each bidirectional rule into two rules, one for forward and one for reverse
+
+            [
+                A or B -> C
+            ],
+            [
+                C or D -> A
+            ],
+            [
+                Aplural or Bplural -> Cplural
+            ],
+            [
+                Cplural or Dplural -> Aplural
+            ]
+
+            Stage 3: Handle multi-input
+
+            Split each rule with multiple inputs into one rule per input (we could handle this within a single regex, but that
+            would make subsequent transformations more difficult. Might be possible if I can find a library that will construct
+            a regex from an 'expression tree' type structure)
+
+            [
+                A -> C
+            ],
+            [
+                B -> C
+            ],
+            [
+                C -> A
+            ],
+            [
+                D -> A
+            ],
+            [
+                Aplural -> Cplural
+            ],
+            [
+                Bplural -> Cplural
+            ],
+            [
+                Cplural -> Aplural
+            ],
+            [
+                Dplural -> Aplural
+            ]
+
+            Stage 4: Handle capitalisation
+
+            Split each rule into multiple rules matching  and outputting different capitalisation cases
+
+            Stage 5: Handle replace via UID
+
+            Split each rule into two rules, one which transforms the input into a UID and another which transforms that UID into the original output.
+            */
+
             Rules = HandlePlurals(Rules);
+            Rules = HandleBidirectional(Rules);
+            Rules = HandleMultiInput(Rules);
 
             IEnumerable<JsonSubstitution> substitutions = HandleCapitalisation(Rules);
 
@@ -46,15 +233,15 @@ internal static class Program
 
             phase1Groups.Add(new JsonReplacementGroup
             {
-                Name = $"{Rule.Input} -> {Rule.Output}",
+                Name = $"{Rule.Inputs[0]} -> {Rule.Output}",
                 Substitutions = phase1substitutions
             });
-            
+
             if(phase2substitutions.Any())
             {
                 phase2Groups.Add(new JsonReplacementGroup
                 {
-                    Name = $"{Rule.Input} -> {Rule.Output}",
+                    Name = $"{Rule.Inputs[0]} -> {Rule.Output}",
                     Substitutions = phase2substitutions
                 });
             }
@@ -83,19 +270,78 @@ internal static class Program
         Console.WriteLine(JsonSerializer.Serialize(JsonRules, options));
     }
 
+    /// <summary>
+    /// Takes unidirectional rules and outputs one rule for each different input entry
+    /// </summary>
+    /// <returns></returns>
+    private static IEnumerable<ReplacementRule> HandleMultiInput(IEnumerable<ReplacementRule> rules)
+    {
+        foreach(var rule in rules)
+        {
+            if(rule.Bidirectional || rule.InputsPlural.Any())
+            {
+                throw new Exception("Rules must be unidirectional");
+            }
+
+            foreach(var (i, input) in rule.Inputs.Index())
+            {
+                yield return rule with { Inputs = [input] };
+            }
+        }
+    }
+
+    private static IEnumerable<ReplacementRule> HandleBidirectional(IEnumerable<ReplacementRule> rules)
+    {
+        foreach(var rule in rules)
+        {
+            if(rule.InputsPlural.Any() || rule.ReverseInputsPlural.Any())
+            {
+                throw new Exception("Rules must not have plural handling");
+            }
+
+            yield return rule with 
+            {
+                Bidirectional = false
+            };
+
+            if(rule.Bidirectional)
+            {
+                if(!rule.ReplaceViaUid)
+                {
+                    throw new Exception();
+                }
+
+                yield return rule with
+                {
+                    Inputs = rule.ReverseInputs,
+                    ReverseInputs = rule.Inputs,
+
+                    Bidirectional = false
+                };
+            }
+        }
+    }
+
     private static IEnumerable<ReplacementRule> HandlePlurals(IEnumerable<ReplacementRule> rules)
     {
         foreach(var rule in rules)
         {
             // Return the rule as-is to handle the singular
-            yield return rule;
+            yield return rule with
+            {
+                InputsPlural = [],
+                ReverseInputsPlural = []
+            };
 
-            if(!string.IsNullOrWhiteSpace(rule.InputPlural) && !string.IsNullOrWhiteSpace(rule.OutputPlural))
+            // Return a rule that handles only the plural
+            if(rule.InputsPlural.Any() && rule.ReverseInputsPlural.Any())
             {
                 yield return rule with
                 {
-                     Input = rule.InputPlural,
-                     Output = rule.OutputPlural
+                    Inputs = rule.InputsPlural,
+                    ReverseInputs = rule.ReverseInputsPlural,
+                    InputsPlural = [],
+                    ReverseInputsPlural = []
                 };
             }
         }
@@ -105,13 +351,20 @@ internal static class Program
     {
         foreach(var rule in rules)
         {
+            if(rule.Inputs.Count > 1 || rule.ReverseInputs.Count > 1 || rule.Bidirectional)
+            {
+                throw new Exception("Rules must have be unidirectional with single input");
+            }
+
+            string input = rule.Inputs[0];
+
             if (rule.CapitalisationHandling == CapitalisationHandling.Preserve)
             {
                 // All uppercase
                 yield return new JsonSubstitution
                 {
                     CaseSensitive = true,
-                    Input = GetRegexForInput(rule, rule.Input.ToUpperInvariant()),
+                    Input = GetRegexForInput(rule, input.ToUpperInvariant()),
                     Output = rule.Output.ToUpperInvariant()
                 };
 
@@ -119,7 +372,7 @@ internal static class Program
                 yield return new JsonSubstitution
                 {
                     CaseSensitive = true,
-                    Input = GetRegexForInput(rule, char.ToUpperInvariant(rule.Input[0]) + string.Concat(rule.Input.Skip(1))),
+                    Input = GetRegexForInput(rule, char.ToUpperInvariant(input[0]) + string.Concat(input.Skip(1))),
                     Output = char.ToUpperInvariant(rule.Output[0]) + string.Concat(rule.Output.Skip(1)),
                 };
 
@@ -127,7 +380,7 @@ internal static class Program
                 yield return new JsonSubstitution
                 {
                     CaseSensitive = false,
-                    Input = GetRegexForInput(rule, rule.Input.ToLowerInvariant()),
+                    Input = GetRegexForInput(rule, input.ToLowerInvariant()),
                     Output = rule.Output.ToLowerInvariant()
                 };
             }
@@ -136,7 +389,7 @@ internal static class Program
                 yield return new JsonSubstitution
                 {
                     CaseSensitive = rule.CapitalisationHandling == CapitalisationHandling.MatchExact,
-                    Input = rule.Input,
+                    Input = input,
                     Output = rule.Output
                 };
             }
--- FoxReplaceRegexGenerator/ReplacementRule.cs
+++ FoxReplaceRegexGenerator/ReplacementRule.cs
@@ -1,27 +1,85 @@
 record ReplacementRule
 {
 
-    public ReplacementRule(string input, string output)
-    {
-        Input = input;
-        Output = output;
-        InputPlural = Input + "s";
-        OutputPlural = Output + "s";
+    public ReplacementRule(string input, string output, string? inputPlural = null, string? outputPlural = null) : this([input], output, inputPlural, outputPlural)
+    {   
     }
 
-    public string Input { get; set; }
-    public string Output { get; set; }
+    public ReplacementRule(IEnumerable<string> inputs, string output, string? inputPlural = null, string? outputPlural = null) : this(inputs, [output])
+    {
+        // If inputPlural is not defined, InputsPlural defaults to inputs with "s" appended. To disable plurals completely, set InputsPlural to empty after construction
+        if(!string.IsNullOrEmpty(inputPlural))
+        {
+            InputsPlural = [inputPlural];
+        }
+        else
+        {
+            InputsPlural = Inputs.Select(x => x + "s").ToList();
+        }
+
+        // If outputPlural is not defined, ReverseInputsPlural defaults to ReverseInputs with "s" appended. To disable plurals completely, set InputsPlural to empty after construction
+        if(!string.IsNullOrEmpty(outputPlural))
+        {
+            ReverseInputsPlural = [outputPlural];
+        }
+        else
+        {
+            ReverseInputsPlural = ReverseInputs.Select(x => x + "s").ToList();
+        }
+    }
+
+    public ReplacementRule(IEnumerable<string> inputs, IEnumerable<string> outputs)
+    {
+        if(!inputs.Any() || !outputs.Any())
+        {
+            throw new Exception("Rules must have at least one input and output");
+        }
+
+        Inputs = inputs.ToList();
+        ReverseInputs = outputs.ToList();
+    }
+
+    // Chainable methods for convenient config
+    public ReplacementRule NoPluralHandling()
+    {
+        InputsPlural = ReverseInputsPlural = [];
+        return this;
+    }
+
+    /// <summary>
+    /// Input strings that should be matched. If Bidirectional is true, the first input is the output of the reverse rule
+    /// </summary>
+    public List<string> Inputs { get; set; }
+    /// <summary>
+    /// Output of the reverse rule, if Bidirectional is true
+    /// </summary>
+    public string ReverseOutput => Inputs[0];
+
+    /// <summary>
+    /// Output of the rule
+    /// </summary>
+    public string Output => ReverseInputs[0];
+    /// <summary>
+    /// Input strings that should be matched by the reverse rule. The first entry is also the output of the forward rule.
+    /// </summary>
+    public List<string> ReverseInputs { get; set; }
+
+    /// <summary>
+    /// If true, a word boundary is required at each end of the input
+    /// </summary>
     public bool WholeWords { get; set; } = true;
     
     /// <summary>
-    /// If neither is null or empty, generated rules will also replace InputPlural with OutputPlural. Defaults to Input + "s"
+    /// If not empty, generated rules will also replace anything in InputsPlural with OutputPlural. Defaults to Input + "s"
     /// </summary>
-    public string InputPlural { get; set; }
+    public List<string> InputsPlural { get; set; } = [];
+
+    public string? OutputPlural => ReverseInputsPlural.Any() ? ReverseInputsPlural[0] : null;
     /// <summary>
     /// If neither is null or empty, generated rules will also replace InputPlural with OutputPlural. Defaults to Output + "s"
     /// </summary>
-    public string OutputPlural { get; set; }
-    public CapitalisationHandling CapitalisationHandling {get; set;} = CapitalisationHandling.Preserve;
+    public List<string> ReverseInputsPlural { get; set; } = [];
+    public CapitalisationHandling CapitalisationHandling { get; set; } = CapitalisationHandling.Preserve;
 
     /// <summary>
     /// If true, two rules will be generated, one that replaces the input with a UID and another with later ordering that replaces the UID with the final result. This ensures that the output of one rule cannot be modified by other rules.
@@ -31,7 +89,6 @@ record ReplacementRule
     /// If true, will also replace the output with the input. Requires ReplaceViaUid, otherwise the second replacement will undo the first
     /// </summary>
     public bool Bidirectional { get; set; } = false;
-
 }
 
 public enum CapitalisationHandling
@@ -41,13 +98,13 @@ public enum CapitalisationHandling
     /// </summary>
     Ignore,
     /// <summary>
-    /// Match only the input capitalisatino specified in the rule, output with capitalisation as given in the rule
+    /// Match only the input capitalisation specified in the rule, output with capitalisation as given in the rule
     /// </summary>
     MatchExact,
     /// <summary>
-    /// Match any input capitalisation, attempt output with matching capitalisation with the following cases in priority order:
+    /// Match any input capitalisation, attempt to output with matching capitalisation with the following cases in priority order:
     /// 1. all uppercase (if input all uppercase)
-    /// 2. first letter only uppercase (else if input starts with uppercase)
+    /// 2. first letter only uppercase (if input starts with uppercase)
     /// 3. all lowercase
     /// </summary>
     Preserve
